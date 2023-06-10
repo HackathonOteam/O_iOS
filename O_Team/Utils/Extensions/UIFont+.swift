ac7023456217f4b66d0c3a1df60174a04b3f87e5
iosdev.sw <iosdev.sw@gmail.com>
2023-06-10 20:03:44 +0900

    Feat:  Add main font

---
+++ O_Team/Utils/Extensions/UIFont+.swift
@@ -0,0 +1,20 @@
+//
+//  UIFont+.swift
+//  O_Team
+//
+//  Created by SangWoo's MacBook on 2023/06/10.
+//
+
+import UIKit
+
+extension UIFont {
+    public enum PretendardType: String {
+        case bold = "Bold"
+        case medium = "Medium"
+        case regular = "Regular"
+    }
+
+    static func pretendard(_ type: PretendardType, size: CGFloat) -> UIFont {
+        return UIFont(name: "Pretendard-\(type.rawValue)", size: size)!
+    }
+}
