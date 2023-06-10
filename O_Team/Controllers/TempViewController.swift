889828d1492b4819410aa9a641379c8e383a44e2
jife98@naver.com <114370871+jife98@users.noreply.github.com>
2023-06-10 15:33:59 +0900

    시작!

---
+++ O_Team/Controllers/TempViewController.swift
@@ -0,0 +1,18 @@
+//
+//  TempViewController.swift
+//  O_Team
+//
+//  Created by 최지철 on 2023/06/10.
+//
+
+import UIKit
+
+class TempViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+   
+
+}
---
+++ O_Team/Models/TempModel.swift
@@ -0,0 +1,8 @@
+//
+//  TempModel.swift
+//  O_Team
+//
+//  Created by 최지철 on 2023/06/10.
+//
+
+import Foundation
