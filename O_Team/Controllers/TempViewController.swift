246f2a51492005618510181ea551371332d55ef6
jife98@naver.com <114370871+jife98@users.noreply.github.com>
2023-06-10 19:14:55 +0900

    라이브러리 업데이트

--- O_Team/Controllers/TempViewController.swift
+++ O_Team/Controllers/TempViewController.swift
@@ -6,13 +6,16 @@
 //
 
 import UIKit
+import SnapKit
 
 class TempViewController: UIViewController {
-
+    let backgroundview = UIView()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        self.view.backgroundColor = .green
 
+        
     }
-   
-
+    
 }
