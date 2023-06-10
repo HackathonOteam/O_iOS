90d4c4e62ba2ce42faeb669e8825089d598609c5
iosdev.sw <iosdev.sw@gmail.com>
2023-06-11 05:03:26 +0900

    Feat: test

---
+++ O_Team/Views/ChatLabel.swift
@@ -0,0 +1,27 @@
+//
+//  ChatLabel.swift
+//  O_Team
+//
+//  Created by SangWoo's MacBook on 2023/06/11.
+//
+
+import UIKit
+
+final class ChatLabel: UILabel {
+    let tailLayer = CAShapeLayer()
+    
+    override func draw(_ rect: CGRect) {
+        super.draw(rect)
+        tailLayer.strokeColor = UIColor.black.cgColor
+        tailLayer.lineDashPattern = [4,2]
+        tailLayer.lineWidth = 1.2
+        tailLayer.frame = self.bounds
+        
+        let path = UIBezierPath()
+        path.move(to: CGPoint(x: 20, y: tailLayer.frame.origin.y))
+        path.addLine(to: CGPoint(x: 20, y: tailLayer.frame.height))
+        
+        self.tailLayer.path = path.cgPath
+        self.tailLayer.insertSublayer(layer, at: 0)
+    }
+}
