2119368842d44fa4b0ad9f313cf76bd4b59d3081
jife98@naver.com <114370871+jife98@users.noreply.github.com>
2023-06-11 09:53:44 +0900

    찐막! jife98

---
+++ O_Team/Service/CalendarService.swift
@@ -0,0 +1,76 @@
+//
+//  CalendarService.swift
+//  O_Team
+//
+//  Created by 최지철 on 2023/06/11.
+//
+
+import Foundation
+import Alamofire
+
+
+struct DiaryMonthly: Codable {
+    let date: String
+    let emotion: String
+}
+
+struct Diary: Codable {
+    let contents: String
+    let date: String
+    let emotion: String
+}
+
+typealias DiaryMonthlyResponse = [DiaryMonthly]
+
+struct DiaryData: Codable {
+    let anniversaryDiary: Diary
+    let positiveDiary: Diary
+    let yearAgoDiary: Diary
+}
+struct DiaryMonthResponse: Codable {
+    let date: String
+    let emotion: String
+}
+class CalendarService{
+    func getHomeVideoList(userName: String, today: String, onCompletion: @escaping (DiaryData?) -> Void) {
+        let url = "http://tarae-env.eba-uepb7id2.ap-northeast-2.elasticbeanstalk.com:80/api/diary?userName=\(userName)&today=\(today)"
+        guard let transfromURL = url.addingPercentEncoding(withAllowedCharacters: .urlQueryAllowed) else {return}
+        AF.request(transfromURL,
+                   method: .get,
+                   parameters: nil,
+                   headers: nil)
+        .responseDecodable(of: DiaryData.self) { response in
+                switch response.result {
+                case .success(let response):
+                   onCompletion(response)
+                case .failure(let error):
+                    print("1번째꺼오류: \(error.localizedDescription)")
+                    onCompletion(nil)
+                }
+            }
+    }
+    func getMonthEmotion(yearMonth: String, userName: String, onCompletion: @escaping (DiaryMonthlyResponse?) -> Void) {
+        let url = "http://tarae-env.eba-uepb7id2.ap-northeast-2.elasticbeanstalk.com:80/api/diary/monthly?yearMonth=\(yearMonth)&userName=\(userName)"
+        guard let transfromURL = url.addingPercentEncoding(withAllowedCharacters: .urlQueryAllowed) else {return}
+        
+        //        let parameter: Parameters = [
+        //            "yearMonth" : yearMonth,
+        //            "userName" : userName]
+        AF.request(transfromURL,
+                   method: .get,
+                   parameters: nil,
+                   encoding: JSONEncoding.default,
+                   headers: nil)
+            .responseDecodable(of: DiaryMonthlyResponse.self) { response in
+                print(response.debugDescription)
+                switch response.result {
+                case .success(let response):
+                    print(response)
+                    onCompletion(response)
+                case .failure(let error):
+                    print("달감정오류: \(error.localizedDescription)")
+                    onCompletion(nil)
+                }
+            }
+    }
+}
